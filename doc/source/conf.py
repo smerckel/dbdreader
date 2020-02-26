ac09d627164e7153f919e5466aad93b35f826a7c
Lucas Merckelbach <lucas.merckelbach@hzg.de>
2020-02-26 12:52:55 +0100

    adding autodoc_mock_imports

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,6 +19,8 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../..'))
 
+autodoc_mock_imports = ["_dbdreader"]
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
