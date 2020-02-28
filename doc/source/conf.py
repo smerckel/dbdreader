663c9a3739be503ae581cd47bb0e1367421fdffa
Lucas Merckelbach <lucas.merckelbach@hzg.de>
2020-02-28 15:13:07 +0100

    still trying...

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 autodoc_mock_imports = ["_dbdreader"]
 
-from dbdreader import __version__
+__version__ = "0.4.0"
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
