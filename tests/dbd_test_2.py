9852c565242543369913d2a97f35443070236a9d
Owain Jones <owanes@noc.ac.uk>
2021-11-19 21:11:39 +0000

    refactor: remove some unneeded test files

--- tests/dbd_test_2.py
+++ tests/dbd_test_2.py
@@ -11,29 +11,32 @@ os.chdir(
 )
 
 
+variable = 'm_battery'
+
+
 print('Big endian file (Slocum G2, Persistor - Motorola MC68CK332)')
 dbds = glob.glob('../data/amadeus*bd')
 dbd = dbdreader.MultiDBD(filenames=dbds, cacheDir='../data/cac')
-timestamps, data = dbd.get('m_battery')
+timestamps, data = dbd.get(variable)
 print('timestamp min =', numpy.min(timestamps))
 print('timestamp avg =', numpy.mean(timestamps))
 print('timestamp max =', numpy.max(timestamps))
-print('data min =', numpy.min(data))
-print('data avg =', numpy.mean(data))
-print('data max =', numpy.max(data))
+print(variable, 'min =', numpy.min(data))
+print(variable, 'avg =', numpy.mean(data))
+print(variable, 'max =', numpy.max(data))
 print()
 
 
 print('Little endian file (Slocum G3, ARM)')
 dbds2 = glob.glob('../data/unit_887*bd')
 dbd2 = dbdreader.MultiDBD(filenames=dbds2, cacheDir='../data/cac')
-timestamps2, data2 = dbd2.get('m_battery')
+timestamps2, data2 = dbd2.get(variable)
 print('timestamp min =', numpy.min(timestamps2))
 print('timestamp avg =', numpy.mean(timestamps2))
 print('timestamp max =', numpy.max(timestamps2))
-print('data min =', numpy.min(data2))
-print('data avg =', numpy.mean(data2))
-print('data max =', numpy.max(data2))
+print(variable, 'min =', numpy.min(data2))
+print(variable, 'avg =', numpy.mean(data2))
+print(variable, 'max =', numpy.max(data2))
 print()
 
 
@@ -50,11 +53,11 @@ for glider_dir in glider_dirs:
     dbd = dbdreader.MultiDBD(
         filenames=dbds, cacheDir=cac_dir
     )
-    timestamps, data = dbd.get('m_battery')
+    timestamps, data = dbd.get(variable)
     print('timestamp min =', numpy.min(timestamps))
     print('timestamp avg =', numpy.mean(timestamps))
     print('timestamp max =', numpy.max(timestamps))
-    print('data min =', numpy.min(data))
-    print('data avg =', numpy.mean(data))
-    print('data max =', numpy.max(data))
+    print(variable, 'min =', numpy.min(data))
+    print(variable, 'avg =', numpy.mean(data))
+    print(variable, 'max =', numpy.max(data))
     print()
