b9a0ec74c6171ca5dbab3326eed38940fecc8a2d
Matthew Cazaly <matcaz@noc.ac.uk>
2022-11-14 09:01:33 +0000

    Add tests for include_source.

--- tests/dbdreader_test.py
+++ tests/dbdreader_test.py
@@ -271,6 +271,32 @@ class Dbdreader_MultiDBD_test(unittest.TestCase):
             data = e.data
             assert data.missing_cache_files['c4ec741e'][0] =="../data/unit_887-2021-321-3-0.tbd"
 
+    def test_include_source_data(self):
+        print("Verify that source DBDs correctly map to data points.")
+        multi = dbdreader.MultiDBD(pattern=self.pattern)
+
+        for parameter in multi.parameterNames["eng"] + multi.parameterNames["sci"]:
+            output = multi.get(parameter, include_source=True)
+            dbds = set(output[2])  # Unique source DBDs for this parameter
+            for dbd in dbds:
+                mask = output[2] == dbd
+                content = [output[0][mask], output[1][mask]]  # Only data attributed to this DBD
+                single = dbd.get(parameter)
+                # Data should be identical
+                assert all(content[0] == single[0])
+                assert all(content[1] == single[1])
+
+    def test_include_source_files(self):
+        print("Verify that all designated files are represented in sources.")
+        files = glob.glob(self.pattern)
+        multi = dbdreader.MultiDBD(pattern=self.pattern)
+
+        output = multi.get(*(multi.parameterNames["eng"] + multi.parameterNames["sci"]), include_source=True)
+
+        sources = {dbd.filename for parameter in output for dbd in parameter[2]}  # Unique source DBDs
+        # All files should be represented
+        assert all(file in sources for file in files)
+        assert all(source in files for source in sources)
         
     def get_method(self,method,fn,x,y):
         dbd=dbdreader.MultiDBD(pattern=fn)
